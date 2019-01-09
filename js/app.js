437e89999fe80e63e764f6c98b70bfa822b01f10
tomaswww <twingord@gmail.com>
2019-01-08 22:40:37 -0300

    Modify function for creating star rate and html for question4, to create stars from js and asign them their eventListener, now must fix the event listener to correct scope for stars

--- js/app.js
+++ js/app.js
@@ -1,13 +1,21 @@
+
+// function that creates stars and asign them their eventlistener... must figure out how to fix click XD
+
+
 function punctuateApp(){
-  var buttonClicked = document.querySelectorAll("fa fa-star");
-  buttonClicked.addEventListener('click',function(){
-    if (buttonClicked.classList.contains("checked")){
-      buttonClicked.classList.remove("checked");
-    }
-    else {
-      buttonClicked.classList.remove("checked")
-    }
-  })
-}
+  for(var i=0;i<5;i++){
+var createStar = document.getElementById("stars");
+var newStar = document.createElement("span");
+newStar.className = "fa fa-star";
+newStar.addEventListener('click',function(){
+  if (newStar.classList.contains("checked")){
+    newStar.classList.remove("checked");
+  }
+  else {
+    newStar.classList.add("checked")
+  }
+});
+createStar.appendChild(newStar);
+}};
 
 punctuateApp();
