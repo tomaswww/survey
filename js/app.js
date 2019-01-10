502e3bc939558774131a13de4ab6d517721c61da
tomaswww <twingord@gmail.com>
2019-01-09 23:55:53 -0300

    displayed two possibilites for js color change in stars both not working. need to dig into closures and if loops

--- js/app.js
+++ js/app.js
@@ -1,21 +1,58 @@
+//
+// // function that creates stars and asign them their eventlistener...
+// // testing clousure with udacity tips on cat clicker
+//
+// function punctuateApp(){
+//   for(var i=0;i<5;i++){
+// var createStar = document.getElementById("stars");
+// var newStar = document.createElement("span");
+// newStar.className = "fa fa-star";
+// createStar.appendChild(newStar);
+// // changing closure
+// newStar.addEventListener('click',function(copyNewStar){
+//   return function(){
+//    if (copyNewStar.classList.contains("checked")){
+//     copyNewStar.classList.remove("checked");
+//   }
+//   else {
+//     copyNewStar.classList.add("checked")
+//   }
+// }
+// })();
+// };
+// };
+//
+// punctuateApp();
 
-// function that creates stars and asign them their eventlistener... must figure out how to fix click XD
+// own thought
 
 
+// star creation
 function punctuateApp(){
   for(var i=0;i<5;i++){
 var createStar = document.getElementById("stars");
 var newStar = document.createElement("span");
 newStar.className = "fa fa-star";
-newStar.addEventListener('click',function(){
-  if (newStar.classList.contains("checked")){
-    newStar.classList.remove("checked");
-  }
-  else {
-    newStar.classList.add("checked")
-  }
-});
+newStar.id = "starN"+i;
 createStar.appendChild(newStar);
 }};
 
 punctuateApp();
+
+
+// gimme some color stars
+function gimmeColor(){
+  for(var i=0;i<5;i++){
+var newStar = document.getElementById("starN"+i);
+newStar.addEventListener('click',function(copyNewStar){
+  return function(copyNewStar){
+   if (copyNewStar.classList.contains("checked")){
+    copyNewStar.classList.remove("checked");
+  }
+  else {
+    copyNewStar.classList.add("checked")
+  };
+}})(newStar);
+}};
+
+gimmeColor();
