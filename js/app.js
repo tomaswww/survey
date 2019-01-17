9fdf96cf2f0e3dee78b2051a7567f12ea1a23703
tomaswww <twingord@gmail.com>
2019-01-16 22:57:59 -0300

    added js for creting table in question6 based on an adjectives array

--- js/app.js
+++ js/app.js
@@ -26,32 +26,85 @@
 
 // own thought
 
+// 
+// // star creation
+// function punctuateApp() {
+//   for (let i = 0; i < 5; i++) {
+//     var createStar = document.getElementById("stars");
+//     var newStar = document.createElement("span");
+//     newStar.className = "fa fa-star";
+//     newStar.id = "starN" + i;
+//     createStar.appendChild(newStar);
+//   }
+// };
+//
+// punctuateApp();
+//
+//
+// // gimme some color stars
+// function gimmeColor() {
+//   for (let i = 0; i < 5; i++) {
+//     var newStar = document.getElementById("starN" + i);
+//     newStar.addEventListener('click', function() {
+//       if (newStar.classList.contains("checked")) {
+//         newStar.classList.remove("checked");
+//       } else {
+//         newStar.classList.add("checked")
+//       };
+//     })
+//   };
+// };
+//
+// gimmeColor();
 
-// star creation
-function punctuateApp(){
-  for(let i=0;i<5;i++){
-var createStar = document.getElementById("stars");
-var newStar = document.createElement("span");
-newStar.className = "fa fa-star";
-newStar.id = "starN"+i;
-createStar.appendChild(newStar);
-}};
-
-punctuateApp();
 
 
-// gimme some color stars
-function gimmeColor(){
-  for(let i=0;i<5;i++){
-var newStar = document.getElementById("starN"+i);
-newStar.addEventListener('click',function(){
-   if (newStar.classList.contains("checked")){
-    newStar.classList.remove("checked");
+
+
+// question6 creating the table for checkboxes
+var adjectivesArray = ["Boring", "Busy", "Calm", "Cheap",
+  "Creative", "Cutting-edge", "Exciting", "Expensive",
+  "Familiar", "Fresh", "Impressive", "Innovative",
+  "Inspiring", "Intimidating", "Old", "Professional",
+  "Trustworthy", "Unprofessional", "Empowering",
+  "Approachable", "Disconnected", "Friendly",
+  "Irrelevant", "Patronizing", "Stable"
+];
+
+
+function createTable() {
+  var lineLenght;
+  // set the line lenght for table
+  if (adjectivesArray.lenght % 5 === 0) {
+    var lineLenght = 4;
+  } else if (adjectivesArray.lenght % 4 === 0) {
+    var lineLenght = 5;
+  } else if (adjectivesArray.lenght % 3 === 0) {
+    var lineLenght = 3;
+  } else {
+    console.log("error dude");
   }
-  else {
-    newStar.classList.add("checked")
-  };
-})};
+var theTable = document.createElement("table");
+var amountOfLines = adjectivesArray.lenght / lineLenght;
+// build table per se
+for (var j = 0; j < amountOfLines; j++) {
+  var newTr = document.createElement("tr");
+  for (var i = 0; i < lineLenght; i++) {
+    var newTd = document.createElement("td");
+    var newInpt = document.createElement("input");
+    newInpt.type = "checkbox";
+    var newSpan = document.createElement("span");
+    newSpan.class = "checkmark";
+    var k = (j * lineLenght + (i + 1));
+    newSpan.innerHTML = adjetiveList[k];
+    newTd.appendChild(newspan);
+    newTd.appendChild(newInpt);
+  }
+  newTr.appendChild(newTd);
+  theTable.appendChild(newTr);
+}
+var tableFrame = document.getElementById("tableCard");
+tableFrame.appendChild(theTable);
 };
 
-gimmeColor();
+createTable();
